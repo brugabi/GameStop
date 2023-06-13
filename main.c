81808879c95358ea4a742094905e2cdf982cb037
Brunna <bgabriellams@gmail.com>
2023-06-13 17:19:40 -0300

    Adjustments
    Adjustments

--- main.c
+++ main.c
@@ -27,34 +27,6 @@ void SetColor(int ForgC)
      }
      return;
  }
- void ClearConsoleToColors(int ForgC, int BackC)
- {
- WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
-               //Get the handle to the current output buffer...
- HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-                     //This is used to reset the carat/cursor to the top left.
- COORD coord = {0, 0};
-                  //A return value... indicating how many chars were written
-                    //   not used but we need to capture this since it will be
-                      //   written anyway (passing NULL causes an access violation).
-  DWORD count;
-
-                               //This is a structure containing all of the console info
-                      // it is used here to find the size of the console.
- CONSOLE_SCREEN_BUFFER_INFO csbi;
-                 //Here we will set the current color
- SetConsoleTextAttribute(hStdOut, wColor);
- if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
- {
-                          //This fills the buffer with a given character (in this case 32=space).
-      FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
-
-      FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
-                          //This will set our cursor position for the next print statement.
-      SetConsoleCursorPosition(hStdOut, coord);
- }
- return;
-}
 
 int menu(){
     int option;
@@ -68,7 +40,7 @@ int menu(){
         printf("4. Change a Title\n");
         printf("5. Delete a Title\n");
         SetColor(4);
-        printf("0. END\n");
+        printf("0. END\n\n");
         SetColor(15);
         printf("Option: ");
         scanf("%d",&option);
@@ -349,7 +321,6 @@ void END() {
 }
 
 int main(){
-    ClearConsoleToColors(0,0);
     int size = 0;
     Game *Inventory = NULL;
     Load_File(&Inventory, &size);
